f73313611152e344c225a4404abc1ea3b025b3e9
Raj Katipally <rkatipally@atpco.net>
2019-05-03 11:22:32 -0600

    added logging

--- src/excercises/object_oriented.py
+++ src/excercises/object_oriented.py
@@ -1,7 +1,11 @@
 from src.excercises.bank_excepton import TransactionException
+import logging
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+logger = logging.getLogger()
 
 
-class BankAccount():
+class BankAccount:
 
     def __init__(self, name, balance):
         self.name = name
@@ -12,23 +16,30 @@ class BankAccount():
             raise TransactionException('Cannot deposit negative amount')
         else:
             self.balance += amount
-            self.display_balance()
+            # self.display_balance()
 
     def withdraw(self, amount):
         if self.balance < amount:
             raise TransactionException('Insufficient funds!!')
         else:
             self.balance -= amount
-            self.display_balance()
+            # self.display_balance()
 
     def display_balance(self):
-        print(f'{self.name}::Your new balance is {self.balance}')
+        logger.info(f'{self.name}::Your new balance is {self.balance}')
+
+    def __str__(self):
+        return f'Owner: {self.name}, Balance: {self.balance}'
 
 
 try:
     raj_account = BankAccount('Raj', 1000)
+    logger.info(raj_account)
     raj_account.deposit(500)
+    logger.info(raj_account)
     raj_account.withdraw(700)
+    logger.info(raj_account)
     raj_account.withdraw(900)
+    logger.info(raj_account)
 except TransactionException as ex:
-    print(ex.args[1])
+    logger.error(ex.args[1])
